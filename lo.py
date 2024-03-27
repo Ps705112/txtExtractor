a54622dacb552d0ba68699ce6555614fd040e287
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2024-03-27 19:06:36 +0530

    Update lo.py

--- lo.py
+++ lo.py
@@ -70,9 +70,9 @@ def pull_run(work, cmds):
 	        print(result) 
 bot = Client(
   "CW",
-  bot_token= "6917084113:AAFOK37UIoEQLtY2HYu5UeF77cIF4BfGnwk",
-  api_id= 27097807 ,
-  api_hash= "9fd790a9cb1f639c921d941621d2959d" 
+  bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH 
 )
 
 @bot.on_message(filters.command(["down"]) & ~filters.edited)
